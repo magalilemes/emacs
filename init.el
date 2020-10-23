21e85ac5425bf4f4506451408a883225dd171e0f
Magali Lemes <magalilemes00@gmail.com>
2020-10-23 13:18:33 -0300

    configuração inicial do emacs

---
+++ init.el
@@ -0,0 +1,56 @@
+;; remover menus
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; remover boas vindas
+(setq inhibit-startup-screen t)
+
+;; remover menus
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; numerar linhas
+(global-linum-mode t)
+
+;; tamanho da fonte
+(set-face-attribute 'default nil :height 125)
+
+;; pacotes
+(require 'package)
+(setq package-enable-at-startup nil); desabilita inicialização
+
+;; MELPA
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(package-initialize) ; inicilizar pacotes
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+ 
+(load-theme 'monokai t) 
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package neotree
+  :ensure t
+  :config
+  (progn
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))) 
+
+;; atalhos
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "M-<down>") 'enlarge-window)
+(global-set-key (kbd "M-<up>") 'shrink-window)
+(global-set-key (kbd "M-<left>") 'enlarge-window-horizontally)
+(global-set-key (kbd "M-<right>") 'shrink-window-horizontally)
+
